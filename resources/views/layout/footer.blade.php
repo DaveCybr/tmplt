413ace13a8bd5cd093510d03fe5578ce7c618384
Ridwan-Z <rhidtzy@gmail.com>
2023-11-01 10:32:05 +0700

    first

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,14 @@
+<div class="container-xxl">
+    <div
+      class="footer-container d-flex align-items-center justify-content-between py-2 flex-md-row flex-column"
+    >
+      <div>
+        ©
+        <script>
+          document.write(new Date().getFullYear());
+        </script>
+        , made with ❤️ by <a href="https://pixinvent.com" target="_blank" class="fw-semibold">SPPD</a>
+      </div>
+     
+    </div>
+  </div>
